f08aef4e131a6ff2257c04cd3974b533e7c379ed
Richard Huang <rickypc@users.noreply.github.com>
2020-03-06 11:04:23 -0800

    Add manifest install and uninstall for Windows.

---
+++ manifest_windows.go
@@ -0,0 +1,86 @@
+// manifest_windows.go - Install and Uninstall manifest file for Windows.
+// Copyright (c) 2018 - 2020  Richard Huang <rickypc@users.noreply.github.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package host
+
+import (
+	"encoding/json"
+	"golang.org/x/sys/windows/registry"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+)
+
+// Install creates native-messaging manifest file on appropriate location and
+// add an entry in windows registry.
+func (h *Host) Install() error {
+	registryName := `Software\Google\Chrome\NativeMessagingHosts\` + h.AppName
+	targetName := filepath.Join(filepath.Dir(h.ExecName), h.AppName+".json")
+
+	manifest, err := json.MarshalIndent(h, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	if err := ioutil.WriteFile(targetName, manifest, 0644); err != nil {
+		return err
+	}
+
+	key, err := registry.OpenKey(registry.CURRENT_USER, registryName, registry.SET_VALUE)
+	if err != nil {
+		return err
+	}
+	defer key.Close()
+
+	if err := key.SetStringValue("", targetName); err != nil {
+		return err
+	}
+
+	log.Printf(`Installed: HKCU\%s`, registryName)
+	return nil
+}
+
+// Uninstall removes entry from windows registry and removes native-messaging
+// manifest file from installed location.
+func (h *Host) Uninstall() error {
+	registryName := `Software\Google\Chrome\NativeMessagingHosts\` + h.AppName
+	targetName := filepath.Join(filepath.Dir(h.ExecName), h.AppName+".json")
+
+	key, err := registry.OpenKey(registry.CURRENT_USER, registryName, registry.SET_VALUE)
+	if err != nil {
+		return err
+	}
+	defer key.Close()
+
+	if err := key.DeleteValue(""); err != nil {
+		// It might never have been installed.
+		log.Print(err)
+	}
+
+	if err := os.Remove(targetName); err != nil {
+		// It might never have been installed.
+		log.Print(err)
+	}
+
+	if err := os.Remove(h.ExecName); err != nil {
+		// It might be locked by current process.
+		log.Print(err)
+	}
+
+	if err := os.Remove(h.ExecName + ".chk"); err != nil {
+		// It might not exist.
+		log.Print(err)
+	}
+
+	log.Printf(`Uninstalled: HKCU\%s`, registryName)
+
+	// Exit gracefully.
+	runtime.Goexit()
+	return nil
+}

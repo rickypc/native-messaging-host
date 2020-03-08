d4794e9f2960218783081f35cfeb08b6267f1660
Richard Huang <rickypc@users.noreply.github.com>
2020-03-07 18:29:38 -0800

    Adjust the tests.

--- host_test.go
+++ host_test.go
@@ -21,9 +21,8 @@ import (
 )
 
 type writer struct {
-	content []byte
-	count   int
-	err     int
+	content    []byte
+	count, err int
 }
 
 func (w *writer) Bytes() []byte {
@@ -33,10 +32,12 @@ func (w *writer) Bytes() []byte {
 func (w *writer) Write(buf []byte) (int, error) {
 	w.count++
 
-	if w.err == 1 && w.count == 1 {
-		return 0, errors.New("header write error")
-	} else if w.err == 2 && w.count == 2 {
-		return 0, errors.New("message write error")
+	if w.count == w.err {
+		if w.err == 1 {
+			return 0, errors.New("header write error")
+		} else if w.err == 2 {
+			return 0, errors.New("message write error")
+		}
 	}
 
 	w.content = append(w.content, buf...)
@@ -49,300 +50,195 @@ func TestHostInit(t *testing.T) {
 	exec, _ := os.Executable()
 	absExec, _ := filepath.EvalSymlinks(exec)
 
-	cases := []struct {
-		name string
-		got  *Host
-		want *Host
-	}{
-		{
-			"with default",
-			(&Host{}).Init(),
-			&Host{
-				AppName:    "native-messaging-host",
-				AppDesc:    "native-messaging-host",
-				AppType:    "stdio",
-				AutoUpdate: false,
-				ExecName:   absExec,
-				ByteOrder:  binary.LittleEndian,
-			},
-		},
-		{
-			"with AppName",
-			(&Host{
-				AppName: "my.app.name",
-			}).Init(),
-			&Host{
-				AppName:    "my.app.name",
-				AppDesc:    "my.app.name",
-				AppType:    "stdio",
-				AutoUpdate: false,
-				ExecName:   absExec,
-				ByteOrder:  binary.LittleEndian,
-			},
-		},
-		{
-			"with AppName, AppDesc",
-			(&Host{
-				AppName: "my.app.name",
-				AppDesc: "Description of my app",
-			}).Init(),
-			&Host{
-				AppName:    "my.app.name",
-				AppDesc:    "Description of my app",
-				AppType:    "stdio",
-				AutoUpdate: false,
-				ExecName:   absExec,
-				ByteOrder:  binary.LittleEndian,
-			},
-		},
-		{
-			"with AppName, AppDesc, AppType",
-			(&Host{
-				AppName: "my.app.name",
-				AppDesc: "Description of my app",
-				AppType: "any",
-			}).Init(),
-			&Host{
-				AppName:    "my.app.name",
-				AppDesc:    "Description of my app",
-				AppType:    "any",
-				AutoUpdate: false,
-				ExecName:   absExec,
-				ByteOrder:  binary.LittleEndian,
-			},
-		},
-		{
-			"with AppName, AppDesc, AppType, ByteOrder",
-			(&Host{
-				AppName:   "my.app.name",
-				AppDesc:   "Description of my app",
-				AppType:   "any",
-				ByteOrder: binary.BigEndian,
-			}).Init(),
-			&Host{
-				AppName:    "my.app.name",
-				AppDesc:    "Description of my app",
-				AppType:    "any",
-				AutoUpdate: false,
-				ExecName:   absExec,
-				ByteOrder:  binary.BigEndian,
-			},
-		},
-		{
-			"with AppName, AppDesc, AppType, ByteOrder, UpdateUrl",
-			(&Host{
-				AppName:   "my.app.name",
-				AppDesc:   "Description of my app",
-				AppType:   "any",
-				ByteOrder: binary.BigEndian,
-				UpdateUrl: "https://www.google.com",
-			}).Init(),
-			&Host{
-				AppName:    "my.app.name",
-				AppDesc:    "Description of my app",
-				AppType:    "any",
-				AutoUpdate: false,
-				ExecName:   absExec,
-				ByteOrder:  binary.BigEndian,
-				UpdateUrl:  "https://www.google.com",
-			},
-		},
-		{
-			"with AppName, AppDesc, AppType, ByteOrder, UpdateUrl, Version",
-			(&Host{
-				AppName:   "my.app.name",
-				AppDesc:   "Description of my app",
-				AppType:   "any",
-				ByteOrder: binary.BigEndian,
-				UpdateUrl: "https://www.google.com",
-				Version:   "0.0.0",
-			}).Init(),
-			&Host{
-				AppName:    "my.app.name",
-				AppDesc:    "Description of my app",
-				AppType:    "any",
-				AutoUpdate: true,
-				ExecName:   absExec,
-				ByteOrder:  binary.BigEndian,
-				UpdateUrl:  "https://www.google.com",
-				Version:    "0.0.0",
-			},
-		},
-	}
-
-	for _, tc := range cases {
-		tc := tc // https://github.com/golang/go/wiki/CommonMistakes#using-goroutines-on-loop-iterator-variables
-		t.Run(tc.name, func(t *testing.T) {
+	compare := func(got *Host, want *Host) func(t *testing.T) {
+		return func(t *testing.T) {
 			t.Parallel()
-			if diff := cmp.Diff(tc.want, tc.got); diff != "" {
+			if diff := cmp.Diff(want, got); diff != "" {
 				t.Errorf("mismatch (-want +got):\n%s", diff)
 			}
-		})
+		}
 	}
+
+	t.Run("with default", compare((&Host{}).Init(), &Host{
+		AppName:    "native-messaging-host",
+		AppDesc:    "native-messaging-host",
+		AppType:    "stdio",
+		AutoUpdate: false,
+		ExecName:   absExec,
+		ByteOrder:  binary.LittleEndian,
+	}))
+
+	t.Run("with AppName", compare((&Host{
+		AppName: "my.app.name",
+	}).Init(), &Host{
+		AppName:    "my.app.name",
+		AppDesc:    "my.app.name",
+		AppType:    "stdio",
+		AutoUpdate: false,
+		ExecName:   absExec,
+		ByteOrder:  binary.LittleEndian,
+	}))
+
+	t.Run("with AppName, AppDesc", compare((&Host{
+		AppName: "my.app.name",
+		AppDesc: "Description of my app",
+	}).Init(), &Host{
+		AppName:    "my.app.name",
+		AppDesc:    "Description of my app",
+		AppType:    "stdio",
+		AutoUpdate: false,
+		ExecName:   absExec,
+		ByteOrder:  binary.LittleEndian,
+	}))
+
+	t.Run("with AppName, AppDesc, AppType", compare((&Host{
+		AppName: "my.app.name",
+		AppDesc: "Description of my app",
+		AppType: "any",
+	}).Init(), &Host{
+		AppName:    "my.app.name",
+		AppDesc:    "Description of my app",
+		AppType:    "any",
+		AutoUpdate: false,
+		ExecName:   absExec,
+		ByteOrder:  binary.LittleEndian,
+	}))
+
+	t.Run("with AppName, AppDesc, AppType, ByteOrder", compare((&Host{
+		AppName:   "my.app.name",
+		AppDesc:   "Description of my app",
+		AppType:   "any",
+		ByteOrder: binary.BigEndian,
+	}).Init(), &Host{
+		AppName:    "my.app.name",
+		AppDesc:    "Description of my app",
+		AppType:    "any",
+		AutoUpdate: false,
+		ExecName:   absExec,
+		ByteOrder:  binary.BigEndian,
+	}))
+
+	t.Run("with AppName, AppDesc, AppType, ByteOrder, UpdateUrl", compare((&Host{
+		AppName:   "my.app.name",
+		AppDesc:   "Description of my app",
+		AppType:   "any",
+		ByteOrder: binary.BigEndian,
+		UpdateUrl: "https://www.google.com",
+	}).Init(), &Host{
+		AppName:    "my.app.name",
+		AppDesc:    "Description of my app",
+		AppType:    "any",
+		AutoUpdate: false,
+		ExecName:   absExec,
+		ByteOrder:  binary.BigEndian,
+		UpdateUrl:  "https://www.google.com",
+	}))
+
+	t.Run("with AppName, AppDesc, AppType, ByteOrder, UpdateUrl, Version", compare((&Host{
+		AppName:   "my.app.name",
+		AppDesc:   "Description of my app",
+		AppType:   "any",
+		ByteOrder: binary.BigEndian,
+		UpdateUrl: "https://www.google.com",
+		Version:   "0.0.0",
+	}).Init(), &Host{
+		AppName:    "my.app.name",
+		AppDesc:    "Description of my app",
+		AppType:    "any",
+		AutoUpdate: true,
+		ExecName:   absExec,
+		ByteOrder:  binary.BigEndian,
+		UpdateUrl:  "https://www.google.com",
+		Version:    "0.0.0",
+	}))
 }
 
 func TestHostOnMessage(t *testing.T) {
 	t.Parallel()
 
-	cases := []struct {
-		name   string
-		err    bool
-		exit   bool
-		reader io.Reader
-		want   *H
-	}{
-		{
-			"with nothing",
-			true,
-			true,
-			bytes.NewReader([]byte("")),
-			&H{},
-		},
-		{
-			"with empty message",
-			false,
-			false,
-			getReader(t, ""),
-			&H{},
-		},
-		{
-			"with empty object",
-			false,
-			false,
-			getReader(t, "{}"),
-			&H{},
-		},
-		{
-			"with valid object",
-			false,
-			false,
-			getReader(t, `{"key":"value"}`),
-			&H{"key": "value"},
-		},
-		{
-			"with invalid object",
-			true,
-			false,
-			getReader(t, `{"key":"value}`),
-			&H{},
-		},
-	}
-
-	for _, tc := range cases {
-		tc := tc // https://github.com/golang/go/wiki/CommonMistakes#using-goroutines-on-loop-iterator-variables
-		t.Run(tc.name, func(t *testing.T) {
+	compare := func(wantErr bool, wantExit bool, message interface{}, want *H) func(t *testing.T) {
+		return func(t *testing.T) {
 			t.Parallel()
 
+			var buf bytes.Buffer
 			exited := false
+			got := &H{}
+			reader := bytes.NewReader([]byte(""))
 
-			if tc.exit {
+			switch message.(type) {
+			case string:
+				header := make([]byte, 4)
+				messageStr := message.(string)
+				binary.LittleEndian.PutUint32(header, (uint32)(len(messageStr)))
+				reader = bytes.NewReader(append(header, []byte(messageStr)...))
+			}
+
+			tee := io.TeeReader(reader, &buf)
+
+			if wantExit {
 				oldRuntimeGoexit := runtimeGoexit
 				defer func() { runtimeGoexit = oldRuntimeGoexit }()
 				runtimeGoexit = func() { exited = true }
 			}
 
-			var buf bytes.Buffer
-			got := &H{}
-			tee := io.TeeReader(tc.reader, &buf)
-
 			if err := (&Host{
 				ByteOrder: binary.LittleEndian,
-			}).OnMessage(tee, got); !tc.err && err != nil {
+			}).OnMessage(tee, got); !wantErr && err != nil {
 				input, _ := ioutil.ReadAll(&buf)
-				t.Fatalf("got error %s: %s", input, err)
-			} else if tc.err && err == nil {
+				t.Fatalf("got error %s: %v", input, err)
+			} else if wantErr && err == nil {
 				input, _ := ioutil.ReadAll(&buf)
 				t.Fatalf("want error: %s", input)
-			} else if tc.exit && !exited {
+			} else if wantExit && !exited {
 				input, _ := ioutil.ReadAll(&buf)
 				t.Fatalf("want exit: %s", input)
 			}
 
-			if diff := cmp.Diff(tc.want, got); diff != "" {
+			if diff := cmp.Diff(want, got); diff != "" {
 				t.Errorf("mismatch (-want +got):\n%s", diff)
 			}
-		})
+		}
 	}
+
+	t.Run("with nothing", compare(true, true, nil, &H{}))
+	t.Run("with empty message", compare(false, false, "", &H{}))
+	t.Run("with empty object", compare(false, false, "{}", &H{}))
+	t.Run("with invalid object", compare(true, false, `{"key":"value}`, &H{}))
+	t.Run("with valid object", compare(false, false, `{"key":"value"}`, &H{"key": "value"}))
 }
 
 func TestHostPostMessage(t *testing.T) {
 	t.Parallel()
 
-	cases := []struct {
-		name    string
-		err     bool
-		message interface{}
-		want    *H
-		writer  *writer
-	}{
-		{
-			"with invalid object",
-			true,
-			make(chan int),
-			&H{},
-			&writer{},
-		},
-		{
-			"with header writer error",
-			true,
-			&H{},
-			&H{},
-			&writer{err: 1},
-		},
-		{
-			"with message writer error",
-			true,
-			&H{},
-			&H{},
-			&writer{err: 2},
-		},
-		{
-			"with empty object",
-			false,
-			&H{},
-			&H{},
-			&writer{},
-		},
-		{
-			"with valid object",
-			false,
-			&H{"key": "value"},
-			&H{"key": "value"},
-			&writer{},
-		},
-	}
-
-	for _, tc := range cases {
-		tc := tc // https://github.com/golang/go/wiki/CommonMistakes#using-goroutines-on-loop-iterator-variables
-		t.Run(tc.name, func(t *testing.T) {
+	compare := func(wantErr bool, message interface{}, want *H, writer *writer) func(t *testing.T) {
+		return func(t *testing.T) {
 			t.Parallel()
 
 			got := &H{}
 
 			if err := (&Host{
 				ByteOrder: binary.LittleEndian,
-			}).PostMessage(tc.writer, tc.message); !tc.err && err != nil {
-				t.Fatalf("got error %+v: %s", tc.message, err)
-			} else if tc.err && err == nil {
-				t.Fatalf("want error: %v", tc.message)
+			}).PostMessage(writer, message); !wantErr && err != nil {
+				t.Fatalf("got error %+v: %v", message, err)
+			} else if wantErr && err == nil {
+				t.Fatalf("want error: %v", message)
 			}
 
-			if !tc.err {
-				if err := json.Unmarshal(tc.writer.Bytes()[4:], got); err != nil {
-					t.Fatalf("unmarshal error %v: %s", tc.message, err)
+			if !wantErr {
+				if err := json.Unmarshal(writer.Bytes()[4:], got); err != nil {
+					t.Fatalf("unmarshal error %v: %v", message, err)
 				}
 			}
 
-			if diff := cmp.Diff(tc.want, got); diff != "" {
+			if diff := cmp.Diff(want, got); diff != "" {
 				t.Errorf("mismatch (-want +got):\n%s", diff)
 			}
-		})
+		}
 	}
-}
 
-func getReader(t *testing.T, message string) io.Reader {
-	t.Helper()
-	buf := make([]byte, 4)
-	binary.LittleEndian.PutUint32(buf, (uint32)(len(message)))
-	return bytes.NewReader(append(buf, []byte(message)...))
+	t.Run("with invalid object", compare(true, make(chan int), &H{}, &writer{}))
+	t.Run("with header writer error", compare(true, &H{}, &H{}, &writer{err: 1}))
+	t.Run("with message writer error", compare(true, &H{}, &H{}, &writer{err: 2}))
+	t.Run("with empty object", compare(false, &H{}, &H{}, &writer{}))
+	t.Run("with valid object", compare(false, &H{"key": "value"}, &H{"key": "value"}, &writer{}))
 }
